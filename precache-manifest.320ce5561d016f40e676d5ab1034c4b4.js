9a740a0321ecb01da95aba3ac64a9ecd49f307d2
Пользователь <Sviatoslv>
2019-07-13 00:58:51 +0300

    Updates

---
+++ precache-manifest.320ce5561d016f40e676d5ab1034c4b4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "909da481bd48ef08483e860e60007b38",
+    "url": "/react_people-table/index.html"
+  },
+  {
+    "revision": "9b73e6305272853aa7eb",
+    "url": "/react_people-table/static/css/main.f722ab3c.chunk.css"
+  },
+  {
+    "revision": "30cb7d462558f724f273",
+    "url": "/react_people-table/static/js/2.f523c886.chunk.js"
+  },
+  {
+    "revision": "9b73e6305272853aa7eb",
+    "url": "/react_people-table/static/js/main.4f34b5ce.chunk.js"
+  },
+  {
+    "revision": "c6b2e506936d419d6d52",
+    "url": "/react_people-table/static/js/runtime~main.b2b73302.js"
+  }
+]);
